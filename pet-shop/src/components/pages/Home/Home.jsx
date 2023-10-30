09a41b7abece6d7eca8d7f519b1236ea92437715
Arno Peyronon <arno.peyronon@astree-software.fr>
2023-10-30 15:07:50 +0100

    manage better style

--- pet-shop/src/components/pages/Home/Home.jsx
+++ pet-shop/src/components/pages/Home/Home.jsx
@@ -5,25 +5,25 @@ const Home = () => {
   const cards = [
     {
       title: "Test 1",
-      description: "Description test 1",
+      description: "Description d'une première card pour faire un test",
       imgSrc:
         "https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA",
     },
     {
       title: "Test 2",
-      description: "Description test 2",
+      description: "Description d'une première card pour faire un test",
       imgSrc:
         "https://imgs.search.brave.com/6yz2O2VMpgQz_QxPjezntKKQCz7JzLGM10nxx8rjT9s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2hvbWUvYmx1cmJz/L3Zpc3VhbHMud2Vi/cA",
     },
     {
       title: "Test 3",
-      description: "Description test 3",
+      description: "Description d'une première card pour faire un test",
       imgSrc:
         "https://imgs.search.brave.com/nhDizHNGc5q-bgrcxRQWu2cu8EIrEHKQxG9Bp6YLkGg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/aWNlLWNyYWNrcy1v/bi1hLWZyb3plbi1z/ZWEuanBnP3dpZHRo/PTEwMDAmZm9ybWF0/PXBqcGcmZXhpZj0w/JmlwdGM9MA",
     },
     {
       title: "Test 2",
-      description: "Description test 2",
+      description: "Description d'une première card pour faire un test",
       imgSrc:
         "https://imgs.search.brave.com/6yz2O2VMpgQz_QxPjezntKKQCz7JzLGM10nxx8rjT9s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2hvbWUvYmx1cmJz/L3Zpc3VhbHMud2Vi/cA",
     },
